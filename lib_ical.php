495a5644193a3465c6fa7bcd2869716ec0a71fc6
Jake Olefsky <offsky@gmail.com>
2013-04-23 12:52:50 -0700

    Initial comit

---
+++ lib_ical.php
@@ -0,0 +1,114 @@
+<?
+
+//converts our repeat strings into iCal RRULEs
+function icalRepeatAdvanced($text) {
+	$repeat = '';
+	$text = trim(strtolower($text));
+	
+	$every = strpos($text,'every');
+	$each = strpos($text,'each');
+	
+	if(($every!==FALSE && $every<5) || ($each!==FALSE && $each<5)) { //Every ...
+		preg_match("/[a-z]* ([0-9]*)([a-z ,]*)/i",$text,$match);
+		if(empty($match[1])) $match[1] = 1;
+		if(empty($match[2])) return "";
+			
+		$repeat = "WEEKLY;BYDAY=";
+		$and = '';
+
+		if(strpos($match[2],'sun')!==FALSE) { $repeat .= $and."SU"; $and=","; }
+		if(strpos($match[2],'mon')!==FALSE) { $repeat .= $and."MO"; $and=","; }
+		if(strpos($match[2],'tue')!==FALSE) { $repeat .= $and."TU"; $and=","; }
+		if(strpos($match[2],'wed')!==FALSE) { $repeat .= $and."WE"; $and=","; }
+		if(strpos($match[2],'thu')!==FALSE) { $repeat .= $and."TH"; $and=","; }
+		if(strpos($match[2],'fri')!==FALSE) { $repeat .= $and."FR"; $and=","; }
+		if(strpos($match[2],'sat')!==FALSE) { $repeat .= $and."SA"; $and=","; }
+		
+		if(strpos($match[2],'day')!==FALSE && empty($and)) $repeat="DAILY";
+		else if(strpos($match[2],'week')!==FALSE) $repeat="WEEKLY";
+		else if(strpos($match[2],'month')!==FALSE) $repeat="MONTHLY";
+		else if(strpos($match[2],'year')!==FALSE) $repeat="YEARLY";
+		
+		if($match[1]>1) $repeat.=";INTERVAL=".$match[1];
+		
+		if(strpos($match[2],'weekday')!==FALSE) $repeat = "WEEKLY;BYDAY=MO,TU,WE,TH,FR";
+		else if(strpos($match[2],'weekend')!==FALSE) $repeat = "WEEKLY;BYDAY=SU,SA";
+		
+	} else { //on the ...
+		
+		if(strpos($text,'first')!==FALSE) $num = 1;
+		else if(strpos($text,'second')!==FALSE) $num = 2;
+		else if(strpos($text,'third')!==FALSE) $num = 3;
+		else if(strpos($text,'fourth')!==FALSE) $num = 4;
+		else if(strpos($text,'fifth')!==FALSE) $num = 5;
+		else if(strpos($text,'last')!==FALSE) $num = -1;
+		else {
+			preg_match("/[a-z ]* ([0-9]*)([a-z ]*)/i",$text,$match);
+			
+			if(empty($match[0])) return icalRepeatAdvanced("Every ".$text);
+			if(empty($match[1])) return icalRepeatAdvanced("Every ".$match[2]);
+			$num = $match[1];
+		}
+		
+		if(strpos($text,'mon')!==FALSE) $day = "MO";
+		if(strpos($text,'tue')!==FALSE) $day = "TU";
+		else if(strpos($text,'wed')!==FALSE) $day = "WE";
+		else if(strpos($text,'thu')!==FALSE) $day = "TH";
+		else if(strpos($text,'fri')!==FALSE) $day = "FR";
+		else if(strpos($text,'sat')!==FALSE) $day = "SA";
+		else if(strpos($text,'sun')!==FALSE) $day = "SU";
+			
+		if($num>5) $num=-1;
+		
+		$repeat = "MONTHLY;BYDAY=".$num.$day;
+	}
+	
+	return $repeat;
+}
+
+//converts iCal RRULEs back into our repeat strings
+function iCalReverseRepeat($freq, $interval, $byday) {
+	$ret = "";	
+	
+	if($freq=="DAILY") {
+		if(empty($interval)) {
+			$ret = "every day";
+		} else { 
+			$ret = "every ".makeDecimal($interval)." days";
+		}
+	} else if($freq=="WEEKLY") {
+	
+		if(!empty($interval)) {
+			$ret = "every ".makeDecimal($interval)." weeks";
+		} else if(!empty($byday)) {
+			$ret = "every ".$byday;
+		} else { 
+			$ret = "every week";
+		}
+		
+	} else if($freq=="MONTHLY") {
+		if(!empty($interval)) {
+			$ret = "every ".makeDecimal($interval)." months";
+		} else if(!empty($byday)) {
+			$day = substr($byday,-2);
+			$num = str_replace($day,"",$byday);
+			$ret = "on the ".$num." ".$day;
+		} else { 
+			$ret = "every month";
+		}
+
+	} else if($freq=="YEARLY") {
+		if(empty($interval)) {
+			$ret = "every year";
+		} else { 
+			$ret = "every ".makeDecimal($interval)." year";
+		}
+	} 
+	
+	$ret = formatRepeat($ret);
+	
+	return $ret;
+}
+
+
+?>
